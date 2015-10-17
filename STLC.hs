b826d698578f6477fc9254c35a05426e19654697
koba-e964 <koba_mac@kobas-MacBook-Air.local>
2015-10-17 17:11:31 +0900

    Add new compiler option

--- STLC.hs
+++ STLC.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleContexts, FlexibleInstances #-}
 module STLC where
 
 import qualified Data.Set as Set
